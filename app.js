2723c40dd055dc8bc8adbd7d87699a4432a3f6aa
TULISCA <161599064+TULISCA@users.noreply.github.com>
2024-04-03 15:47:38 -0300

    Update app.js
    retorno

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ function sortear(){
   let quantidade = parseInt(document.getElementById('quantidade').value);
   let de = parseInt(document.getElementById('de').value);
   let ate = parseInt(document.getElementById('ate').value);
-  
+  if (de >= ate) {
+    alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
+    return;
+  }
   let sorteados = [];
   let numeros
   for(let i = 0; i < quantidade; i++){
